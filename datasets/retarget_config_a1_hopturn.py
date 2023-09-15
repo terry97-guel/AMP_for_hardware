1f313364c561855957eedf952e6105ae32dee6e8
terry <taerimyoon@korea.ac.kr>
2023-09-15 15:14:10 +0900

    hopturn motion retarget trial

---
+++ datasets/retarget_config_a1_hopturn.py
@@ -0,0 +1,49 @@
+import numpy as np
+from legged_gym import LEGGED_GYM_ROOT_DIR
+
+
+VISUALIZE_RETARGETING = True
+
+URDF_FILENAME = "{LEGGED_GYM_ROOT_DIR}/resources/robots/a1/urdf/a1.urdf".format(
+    LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR)
+
+OUTPUT_DIR = "{LEGGED_GYM_ROOT_DIR}/datasets/hopturn_a1".format(
+    LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR)
+
+REF_POS_SCALE = 0.825
+INIT_POS = np.array([0, 0, 0.27])
+INIT_ROT = np.array([0, 0, 0, 1.0])
+
+SIM_TOE_JOINT_IDS = [6, 11, 16, 21]
+SIM_HIP_JOINT_IDS = [2, 7, 12, 17]
+SIM_ROOT_OFFSET = np.array([0, 0, -0.04])
+SIM_TOE_OFFSET_LOCAL = [
+    np.array([0, 0.06, 0.0]),
+    np.array([0, -0.06, 0.0]),
+    np.array([0, 0.06, 0.0]),
+    np.array([0, -0.06, 0.0])
+]
+TOE_HEIGHT_OFFSET = 0.02
+
+DEFAULT_JOINT_POSE = np.array([0, 0.9, -1.8, 0, 0.9, -1.8, 0, 0.9, -1.8, 0, 0.9, -1.8])
+JOINT_DAMPING = [0.1, 0.05, 0.01,
+                 0.1, 0.05, 0.01,
+                 0.1, 0.05, 0.01,
+                 0.1, 0.05, 0.01]
+
+FORWARD_DIR_OFFSET = np.array([0, 0, 0])
+
+FR_FOOT_NAME = "FR_foot"
+FL_FOOT_NAME = "FL_foot"
+HR_FOOT_NAME = "RR_foot"
+HL_FOOT_NAME = "RL_foot"
+
+MOCAP_MOTIONS = [
+    # Output motion name, input file, frame start, frame end, motion weight.
+    [
+        "hopturn",
+        "{LEGGED_GYM_ROOT_DIR}/datasets/hopturn/hopturn.txt".format(
+            LEGGED_GYM_ROOT_DIR=LEGGED_GYM_ROOT_DIR), 0, 80, 1
+
+    ]
+]
