a446141eadab88df74ec3bdf9658f5d4e4940f16
terry <taerimyoon@korea.ac.kr>
2023-10-17 20:01:25 +0900

    deployment code

---
+++ go1_gym_deploy/temp.sh
@@ -0,0 +1,14 @@
+docker run -d\
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=${XAUTH}" \
+    --volume="${XAUTH}:${XAUTH}" \
+    --volume="/home/unitree/go1_gym:/home/isaac/go1_gym" \
+    --privileged \
+    --runtime=nvidia \
+    --net=host \
+    --workdir="/home/isaac/go1_gym" \
+    --name="foxy_controller" \
+    jetson-model-deployment tail -f /dev/null
+    docker start foxy_controller
